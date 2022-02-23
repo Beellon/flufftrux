99943ac4376dba326dd65dbff7ea85863310a928
Fluffyx <76226827+Beellon@users.noreply.github.com>
2022-02-23 11:10:12 +0100

    Update flufftruxv1.lua

--- flufftruxv1.lua
+++ flufftruxv1.lua
@@ -1,4 +1,4 @@
-if not isfolder("flufftrox") then makefolder("testthing") end
+if not isfolder("flufftrox") then makefolder("flufftrox") end
 if isfile('flufftrox.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = 
 game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "G6JCA4Wk"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile('flufftrox.txt', "discord")})end
 
